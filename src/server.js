ff58d78c8d51f107736097758bdeb54c76fff017
Hassan <hassanisavailable@gmail.com>
2026-05-12 19:45:38 +0500

    Database setup: - Set up Prisma ORM - Added DB healthcheck endpoint - Removed API setup from Docker to reduce complexity

--- src/server.js
+++ src/server.js
@@ -12,14 +12,35 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// JSON response formatting
+app.set('json spaces', 2);
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/health", (req, res) => {
+app.get("/health/server", (req, res) => {
     res.status(200).json({
         status: "OK",
         message: "Server is healthy",
         timestamp: new Date().toISOString(),
     });
+});
+
+app.get("/health/db", (req, res) => {
+    try {
+        Prisma.$queryRaw`SELECT 1`;
+        res.status(200).json({
+            status: "OK",
+            message: "Database connection is healthy",
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "ERROR",
+            message: "Database connection failed",
+            error: error.message,
+            timestamp: new Date().toISOString(),
+        });
+    }
 });
